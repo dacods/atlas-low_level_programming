fe66a0717e03f7541be54dd5190bbb851661cfb5
dacods <dacods.21@gmail.com>
2024-01-20 21:33:41 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char letters;
+
+for (letters = 'a'; letters <= 'z'; letters++){
+	putchar(letters);
+}
+putchar('\n');
+	
+
+return 0;
+}
