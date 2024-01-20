bf306a3691102c294300a8ba7f855595e0396b62
dacods <dacods.21@gmail.com>
2024-01-20 20:34:38 +0000

    last digit*

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -6,19 +6,22 @@
  */
 int main(void)
 {
-int n;
-char string[] = "Last digit of";
-char is[] = "is";
-char greater[] = "and is greater than 5";
-char zero[] = "and is 0";
-char less[] = "and is less than 6 and not 0";
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 5)
-printf("%s %d %s %d %s\n", string, n, is, n % 10, greater);
-else if (n == 0)
-printf("%s %d %s %d %s\n", string, n, is, n % 10, zero);
-else
-printf("%s %d %s %d %s\n", string, n, is, n % 10, less);
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
+	return (0);
 }
