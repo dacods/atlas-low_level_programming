0063abab82bf1097c746a6d4e137097fe9110fe3
dacods <dacods.21@gmail.com>
2024-01-23 00:58:56 +0000

    98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - This prints all natural numbers up to 98.
+ * @n: the integer
+ * Return: print_to_98
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98){
+		while (n <= 98){
+			printf("%d", n);
+			if (n != 98){
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	else if (n > 98){
+		while (n >= 98){
+			printf("%d", n);
+			if (n != 98){
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else{
+		printf("98");
+	}
+	printf("\n");
+}
