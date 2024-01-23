7fa4b400edc85159b58e5a272a6e34ddac282394
dacods <dacods.21@gmail.com>
2024-01-23 00:09:16 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers 
+ * @num: first integer
+ * @num2: second integer
+ * Return: the result of addition
+ */
+int add(int num, int num2)
+{
+	return (num + num2);
+}
