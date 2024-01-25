25e9b183fec5e978db1bbde8a724234476c04138
dacods <dacods.21@gmail.com>
2024-01-25 22:32:36 +0000

    isupper**

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isupper -
- * @c:
- * Return:
+ * _isupper - checks for uppercase character.
+ * @c: char integer
+ * Return: 1 if upper and 0 if otherewise.
  */
 
 int _isupper(int c)
