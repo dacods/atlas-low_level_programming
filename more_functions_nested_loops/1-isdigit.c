f18975a6bb58b339ca3a982a87c5722fcc05d8f5
dacods <dacods.21@gmail.com>
2024-01-26 19:10:25 +0000

    mul

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit
+ * @c:
+ * Return:
+ */
+int _isdigit(int c)
+{
+	if (c == 'c')
+	{
+		while ((c >= 0) && (c <= 9))
+		{
+			return (0);
+		}
+	return (1);
+	}
+}
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul -
+ * @a:
+ * @b:
+ * Return:
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
