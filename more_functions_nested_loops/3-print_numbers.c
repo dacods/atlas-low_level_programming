fc3cdd196d8c4d4a00f0b5341f9e468810d20bc7
dacods <dacods.21@gmail.com>
2024-01-27 07:33:49 +0000

    print_numbers**

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * print_numbers: prints 1 - 9
+ * print_numbers - prints 1 - 9
  *
- * Return: 0
+ * Return:  1 - 9
  */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		_putchar(i);
