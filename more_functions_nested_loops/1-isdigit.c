76e0532cf9c7900c696610f172d0c87c3a64c87d
dacods <dacods.21@gmail.com>
2024-01-27 21:43:12 +0000

    isdigit*

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _isdigit
- * @c:
- * Return:
+ * _isdigit - checks for a digit
+ * @c: integer
+ * Return: 1 if c is present 0 if otherwise.
  */
 int _isdigit(int c)
 {
