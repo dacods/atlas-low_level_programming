3c9db192c35dade8fcc83feadfd1ecd60bbba522
dacods <dacods.21@gmail.com>
2024-01-28 07:27:08 +0000

    more_numbers****

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,16 @@
  */
 void more_numbers(void)
 {
-	int num, i;
+int num, i;
 
-	for (i = 0; i < 10; i++)
-	{
-	for (num = 0; num <= 14; num++)
-	{
+for (i = 0; i < 10; i++)
+{
+for (num = 0; num <= 14; num++)
+{
 if (num >= 10)
 _putchar('0' + num / 10);
 _putchar('0' + num % 10);
-	}
-		_putchar('\n');
-	}
+}
+_putchar('\n');
+}
 }
