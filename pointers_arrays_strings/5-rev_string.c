3cb058604d15ac95dcfac85060d0a0a7bbb8c406
dacods <dacods.21@gmail.com>
2024-01-29 20:57:09 +0000

    rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	char p = s[0];
+	int i = 0;
+	int l;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	
+	for (l = 0; l < i; l++)
+	{
+		i--;
+		p = s[l];
+		s[l] = s[i];
+		s[i] = p;
+	}
+}
