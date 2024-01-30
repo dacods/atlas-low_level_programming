a9e9d1f8083b607386dda5a9b8730d7097730720
dacods <dacods.21@gmail.com>
2024-01-30 00:24:03 +0000

    puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int l;
+	int c;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		c++;
+	}
+	l = ( c / 2);
+	if ((c % 2) == 1)
+	{
+		l = ((c + 1) / 2);
+	}
+	for (i = l; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
