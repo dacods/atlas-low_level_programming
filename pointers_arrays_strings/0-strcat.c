83f6b9eec8e6f1b62d4131177516ee2670a411de
dacods <dacods.21@gmail.com>
2024-02-03 23:03:30 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,21 @@
 
 char *_strcat(char *dest, char *src)
 {
+	char *newString = dest;
 
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+
+	return (newString);
 }
