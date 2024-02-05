f9dcdee6d1120f366cb7c08c3a9effb68c53ff5e
dacods <dacods.21@gmail.com>
2024-02-05 01:20:55 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stddef.h>
 /**
- * _strncpy -
- * @dest:
- * @src:
- * @n:
- * Return:
+ * _strncpy - copies a string
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
