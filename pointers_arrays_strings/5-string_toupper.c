b52d0afda6a169b81ca8e76e3443fda8f381c024
dacods <dacods.21@gmail.com>
2024-02-05 23:58:38 +0000

    string_toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * string_toupper -
- * 
- * Return:
+ * string_toupper - changes all lowercase to uppercase
+ * @str: string
+ * Return: str
  */
 
 char *string_toupper(char *str)
 {
-	while (*str != '\0')
-	{
-		if (*str >= 'a' && *str <= 'z')
-		{
-			*str = *str - ('a' - 'A');
-		}
-		str++;
-	}
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+	}
 	return (str);
 }
