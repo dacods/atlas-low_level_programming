25f431d77865a8f4b31c13fd91bfc0cbc6d53e06
dacods <dacods.21@gmail.com>
2024-02-05 23:01:26 +0000

    rev_array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array
+ * @a: integer
+ * @n: integer
+ * Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *beginning = a;
+	int *ending = a + n - 1;
+	int temp;
+	
+	while (beginning < ending)
+	{
+		temp = *beginning;
+		*beginning = *ending;
+		*ending = temp;
+		beginning++;
+		ending--;
+	}
+}
