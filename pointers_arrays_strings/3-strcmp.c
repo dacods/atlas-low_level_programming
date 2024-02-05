3820f589d989127a40aaf01a248838f0f56b3d80
dacods <dacods.21@gmail.com>
2024-02-05 02:12:06 +0000

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,13 @@ int _strcmp(char *s1, char *s2)
 {
 	char *op1 = s1;
 	char *op2 = s2;
-	
+
 
 	while (*op1 != '\0' && *op2 != '\0')
 	{
 		if (*op1 != *op2)
 		{
-			return *op1 - *op2;
+			return (*op1 - *op2);
 		}
 		op1++;
 		op2++;
