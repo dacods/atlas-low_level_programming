2d3d0f214cfe4f6b0a5832ce9f28d61621343bda
dacods <dacods.21@gmail.com>
2024-02-05 01:17:33 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stddef.h>
 /**
  * _strncpy -
  * @dest:
@@ -8,3 +8,21 @@
  * Return:
  */
 char *_strncpy(char *dest, char *src, int n)
+{
+	char *d = dest;
+	char *s = src;
+
+	while (n > 0 && *s != '\0')
+	{
+		*d++ = *s++;
+		n--;
+	}
+
+	while (n > 0)
+	{
+		*d++ = '\0';
+		n--;
+	}
+
+	return dest;
+}
