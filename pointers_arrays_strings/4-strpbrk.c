666c22cdad5612eee618a907bcdf92a70ef8b819
dacods <dacods.21@gmail.com>
2024-02-06 05:13:46 +0000

    strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,18 @@
 #include "main.h"
 
+/**
+ * _strpbrk - search a string of any set of bytes
+ * @s: pointer
+ * @accept: pointer
+ * Return: 0
+ */
+
 char *_strpbrk(char *s, char *accept)
 {
 	while (*s != '\0')
 	{
 		char *p = accept;
+
 		while (*p != '\0')
 		{
 			if (*s == *p)
