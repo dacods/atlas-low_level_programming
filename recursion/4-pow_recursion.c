d79e23007ea71f0b60383498abb633ed53422e5c
dacods <dacods.21@gmail.com>
2024-02-09 22:44:19 +0000

    pow_recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of a number raised to the power of another number
+ * @x: number
+ * @y: power number
+ * Return: value of x
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y <= 0)
+	{
+		if (y < 0)
+		{
+			return (-1);
+		}
+		return (1);
+	}
+
+	if (x == 0)
+	{
+		return (0);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
