6df033828ec3dbb411a6a14232d3e505023b00b9
dacods <dacods.21@gmail.com>
2024-02-09 19:25:38 +0000

    puts_recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: character
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_puts_recursion(s + 1);
+}
