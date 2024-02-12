f5f77e15e1164f17726f34ffa49cd6bac53fa952
dacods <dacods.21@gmail.com>
2024-02-12 00:43:44 +0000

    whatsmyname

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,17 +3,18 @@
 
 /**
  * main - program that prints its name even if changed
- * argc:
- * argv: 
+ * argc: strings
+ * argv: program name
  * Return: program name
  */
 
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
