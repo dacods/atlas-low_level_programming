38dc3ea84795d1ba058002df139c8d4da16ab882
dacods <dacods.21@gmail.com>
2024-02-12 23:38:51 +0000

    sqrt_recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,31 +1,27 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns natural square root of a number
+ * _sqrt - gets root
+ * @x: number
+ * @y: number
+ * Return: the square root
+ */
+
+int _sqrt(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y > x)
+		return (-1);
+	return (_sqrt(x, y + 1));
+}
+/**
+ * _sqrt_recursion - gets the natural square root of a number
  * @n: number
  * Return: the square root
  */
 
 int _sqrt_recursion(int n)
 {
-	int i;
-
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	else
-	{
-		i = 1;
-		while (i * i <= n)
-		{
-			i++;
-		}
-		i--;
-		return (i);
-	}
+	return (_sqrt(n, 1));
 }
