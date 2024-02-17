bbc29bcc036b2897bbbe3adbd73d12b89a7da68d
dacods <dacods.21@gmail.com>
2024-02-17 00:30:57 +0000

    str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,12 +16,12 @@ char *str_concat(char *s1, char *s2)
 	char *temp2 = s2;
 	char *joined;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 	{
 		s1 = "";
 	}
 
-	if (s2 == 0)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
