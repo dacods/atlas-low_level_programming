02ed39457e10d645e5382d59e3ea8bf6428e10a9
dacods <dacods.21@gmail.com>
2024-02-17 23:23:06 +0000

    malloc checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: integer
+ * Return: 0
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
