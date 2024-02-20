23a0d36730825a670c684479731ed768c434e9cc
dacods <dacods.21@gmail.com>
2024-02-20 01:19:43 +0000

    array range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min:
+ * @max:
+ * Return:
+ */
+
+int *array_range(int min, int max)
+{
+	int *i, l;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	i = (int *)malloc((max - min + 1) * sizeof(int));
+
+	if (i == NULL)
+	{
+		return (NULL);
+	}
+
+	for (l = 0; l<= max - min; l++)
+	{
+		i[l] = min + l;
+	}
+
+	return (i);
+}
