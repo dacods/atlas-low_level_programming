2c6c24778b4d199de82667794a7ca3740380045b
dacods <dacods.21@gmail.com>
2024-02-22 19:50:20 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * dog - new struct
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ * Return: dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
