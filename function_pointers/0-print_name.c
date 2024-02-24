1a2c42336148e65d7be1c77fdf30f72f624d2fa6
dacods <dacods.21@gmail.com>
2024-02-24 22:18:34 +0000

    print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
-#include "main.h"
+#include "function_pointers.h"
 #include <stdlib.h>
 
 /**
  * print_name - prints a name
  * @name: a name
+ * @f: function pointer
  * Return: 0
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-
+	(*f)(name);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef function_pointers_h
+#define function_pointers_h
 
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
