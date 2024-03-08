0460b9daa49bb0e14ab3186c2da550c30b2f461f
dacods <dacods.21@gmail.com>
2024-03-08 21:38:16 +0000

    list len

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include "stdio.h"
 
+/**
+ * list_len - function that returns the number of elements in a linked list
+ * @h: pointer
+ * Return: count
+ */
+
 size_t list_len(const list_t *h)
 {
 	int count = 0;
