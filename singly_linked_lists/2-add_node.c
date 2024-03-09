0460a5ea8ac96b10f2aca39cdb254b3a1089de0e
dacods <dacods.21@gmail.com>
2024-03-09 21:03:05 +0000

    add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,8 +1,47 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - Function that adds a new node at the beginning of a list
+ * @head: pointer
+ * @str: string
+ * Return: new_node
+ */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	struct Node* new_node = (struct Node*)maloc(sizeof(struct Node));
+	list_t *new_node;
+	int count = 0;
+	const char *ptr = str;
 
-	new_node->
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	while (*ptr != '\0')
+	{
+		count++;
+		ptr++;
+	}
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = count;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+
+}
