a1423a7ae55b34984923f762bb5b4d2b73a439d7
dacods <dacods.21@gmail.com>
2024-03-10 18:06:16 +0000

    print listint

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Function that prints all the elements of a list
+ * @h: pointer
+ * Return: count
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+	const listint_t *current = h;
+
+	while (current)
+	{
+		printf("%u\n", current->n);
+
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
