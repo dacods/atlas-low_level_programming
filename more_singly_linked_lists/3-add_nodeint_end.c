3493ec138871e5dcfe7720188fb75c67d0aca663
dacods <dacods.21@gmail.com>
2024-03-10 19:16:25 +0000

    add nodeint end

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - Funtion that adds a new node to the end of a list
+ * @head: pointer
+ * @n: integer
+ * Return: new_node
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint_t *last_node = *head;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = new_node;
+	}
+	return (new_node);
+}
