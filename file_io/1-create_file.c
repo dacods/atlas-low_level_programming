4c798b94a28ad18e6ccba530a687d1358d20bf70
dacods <dacods.21@gmail.com>
2024-03-11 21:35:56 +0000

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,23 +10,19 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t len;
-	
+	ssize_t bytes_written;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
-		{
-			len++;
-		}
-		if (write(fd, text_content, len) == -1)
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
 		{
 			close(fd);
 			return (-1);
