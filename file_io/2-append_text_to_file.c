b88e78d5658cd13e628c23f5ad2da9e2218b88df
dacods <dacods.21@gmail.com>
2024-03-13 17:15:09 +0000

    append text to file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,28 +9,28 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
+	int fd;
 	size_t result;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content == NULL)
+		return (1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
-
-	if (file == 1)
-		return (-1);
-
-	result = write(file, text_content, strlen(text_content));
+	result = write(fd, text_content, strlen(text_content));
 
 	if (result != strlen(text_content))
 	{
-		close(file);
+		close(fd);
 		return (-1);
 	}
-	close(file);
+	close(fd);
 
 	return (1);
 }
