543a7fc14d2118d26e73600ae73c03926bfa7ce0
dacods <dacods.21@gmail.com>
2024-03-14 14:54:11 -0500

    1

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,5 +8,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	
+	int count = 0;
+    size_t *current = h;
+
+    while (current != h)
+    {
+        if (current == NULL)
+            return (0);
+
+        count++;
+    }
+    return (count);
 }
