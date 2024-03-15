b004a6102693aea6aa35bf6e3cce2707b722ca70
dacods <dacods.21@gmail.com>
2024-03-14 22:52:28 -0500

    print dlistint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    int count = 0;
+	int count = 0;
 
-    while (h)
-    {
-        if (h == NULL)
-            return (0);
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
+	while (h)
+	{
+		if (h == NULL)
+			return (0);
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
     }
-    return (count);
+	return (count);
 }
