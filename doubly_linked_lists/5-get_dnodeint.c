80177341f31acd66c44d7da277cd7d634b1cc9d5
dacods <dacods.21@gmail.com>
2024-03-16 20:28:51 +0000

    get dnodeint

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Function that retruns the nth node of a list
+ * @head: head
+ * @index: integer
+ * Return:
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			return (head);
+		}
+		count++;
+		head = head->next;
+	}
+
+	return (NULL);
+}
