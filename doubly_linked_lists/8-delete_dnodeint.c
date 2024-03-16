ff4d969dddf4457799547765ebce409f88737c15
dacods <dacods.21@gmail.com>
2024-03-16 21:18:27 +0000

    delete dnodeint

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Function that deletes the node of a list
+ * @head: head
+ * @index: integer
+ * Return: 1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+	dlistint_t *prev = NULL;
+	unsigned int count = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(current);
+		return (1);
+	}
+
+	while (current != NULL && count < index)
+	{
+		prev = current;
+		current = current->next;
+		count++;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	prev->next = current->next;
+	free(current);
+
+	return (1);
+}
