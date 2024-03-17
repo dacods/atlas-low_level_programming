1578d32530c66835107fab589537b8449a4d2f73
dacods <dacods.21@gmail.com>
2024-03-17 21:42:53 +0000

    key index

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Function that gives you the index of a key
+ * @key: key
+ * @size: size of the array
+ * Return:
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int value = hash_djb2(key);
+
+	return (value % size);
+}
