ec24b07b809fb0b819b1f4062b5adf1a447d3a66
dacods <dacods.21@gmail.com>
2024-03-17 22:41:49 +0000

    hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,14 +12,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new_node;
+	hash_node_t *current;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-	{
 		return (0);
-	}
 
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
@@ -28,7 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	new_node->value = strdup(value);
+	new_node->value = (value != NULL) ? strdup(value) : NULL;
 	if (value != NULL && new_node->value == NULL)
 	{
 		free(new_node->key);
@@ -36,8 +36,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	if (ht->array[index] != NULL)
+	{
+		current = ht->array[index];
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	else
+		ht->array[index] = new_node;
 
 	return (1);
 }
