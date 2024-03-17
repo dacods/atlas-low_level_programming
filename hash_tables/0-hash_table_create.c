64c065913a31c465187417e9b5e405c246ef3cb2
dacods <dacods.21@gmail.com>
2024-03-17 21:13:37 +0000

    hash table create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,7 +3,7 @@
 /**
  * hash_table_create - Function that creates a hash table
  * @size: size of array
- * Return:
+ * Return: hash_table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
