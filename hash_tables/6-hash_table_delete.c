f7710c7621a9680d6574adb1f7c7ec834ea922e7
dacods <dacods.21@gmail.com>
2024-03-18 21:11:33 +0000

    hash table delete

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Function that deletes a hash table
+ * @ht: hash table
+ * Return: 0
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current, *temp;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current;
+			current = current->next;
+			free(temp);
+		}
+	}
+
+	free(ht->array);
+
+	free(ht);
+}
