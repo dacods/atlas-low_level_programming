d4663557bcfce325ea5a67be24d59670c94577a5
dacods <dacods.21@gmail.com>
2024-03-18 20:13:28 +0000

    hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -5,53 +5,49 @@
  * @ht: hash table
  * @key: key
  * @value: value associated with key
- * Return:
+ * Return: 1
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new_node;
-	hash_node_t *current;
-	char *new_value;
+	hash_node_t *new_node, *current;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
-
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = malloc(sizeof(hash_node_t));
+		if (ht->array[index] == NULL)
+			return (0);
+		ht->array[index]->next = NULL;
+	}
 	current = ht->array[index];
-
-	for (; current != NULL; current = current->next)
+	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-			{
-				return (0);
-			}
 			free(current->value);
-			current->value = new_value;
+			current->value = strdup(value);
+			if (current->value == NULL)
+				return (0);
 			return (1);
 		}
+		current = current->next;
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-	{
 		return (0);
-	}
-
+	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-
-	if (new_node->value == NULL)
+	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
