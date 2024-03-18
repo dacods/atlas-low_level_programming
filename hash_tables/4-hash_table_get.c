f86f30ec9e6d612a0f4e4e22a3ae35033729d89c
dacods <dacods.21@gmail.com>
2024-03-18 20:50:10 +0000

    hash table get

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Function that retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key
+ * Return: value associated with the element or NULL if key couldnt be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size);
+
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+			return (current->value);
+		current = current->next;
+	}
+	return (NULL);
+}
