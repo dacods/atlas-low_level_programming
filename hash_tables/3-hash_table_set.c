06fc9775c15c00930431d792838d656763f92b20
dacods <dacods.21@gmail.com>
2024-03-18 19:41:21 +0000

    hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,39 +14,42 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	hash_node_t *current;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
+
+	current = ht->array[index];
+
+	for (; current != NULL; current = current->next)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			if (current->value == NULL)
+			{
+				return (0);
+			}
+			return (1);
+		}
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
+	new_node->value = strdup(value);
 
-	new_node->value = (value != NULL) ? strdup(value) : NULL;
-	if (value != NULL && new_node->value == NULL)
+	if (new_node->key == NULL || new_node->value == NULL)
 	{
 		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
 
-	if (ht->array[index] != NULL)
-	{
-		current = ht->array[index];
-		while (current->next != NULL)
-		{
-			current = current->next;
-		}
-		current->next = new_node;
-	}
-	else
-		ht->array[index] = new_node;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
