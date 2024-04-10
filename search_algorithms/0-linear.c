b33e4d08e15593c22b5d4def0bedcccba419de60
dacods <dacods.21@gmail.com>
2024-04-10 22:28:37 +0000

    Fixed betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -5,7 +5,7 @@
  * @array: Pointer to the first element of the array to search
  * @size: Number of elements
  * @value: Value to search for
- * Return:
+ * Return: The value if found and -1 if not
  */
 
 int linear_search(int *array, size_t size, int value)
