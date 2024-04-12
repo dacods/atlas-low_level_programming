0af6b79fdb758e9bef1e455d04d60705d5df31cb
dacods <135403583+dacods@users.noreply.github.com>
2024-04-12 17:04:05 -0500

    Fixed print statements in 1-binary_search

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -10,23 +10,11 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, mid;
-	size_t i = 0;
-
-	/*printf("Searching in array: ");*/
-
-	/*for (i = left; i <= right; i++)
-	{
-		printf("%d", array[i]);
-		if (i < right)
-			printf(", ");
-	}
-	printf("\n");*/
+	int left = 0, right = size - 1, mid;
+	int i = 0;
 
 	while (left <= right)
 	{
-		mid = left + (right - left) / 2;
-
 		if (array[mid] == value)
 			return (mid);
 
@@ -39,11 +27,14 @@ int binary_search(int *array, size_t size, int value)
 		}
 		printf("\n");
 
+		mid = left + (right - left) / 2;
+		
 		if (array[mid] < value)
 			left = mid + 1;
 		else
 			right = mid - 1;
 	}
+	
 
 	return (-1);
 	
